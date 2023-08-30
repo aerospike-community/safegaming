a772f7d834bdd73b03244339ab56171addd03bce
Richard Andersen <randersenyb@andersenfamily.us>
2023-08-30 19:07:20 +0000

    Updated chmod and project

--- GameDashBoard/Aerospike/publishrelease-linux.sh
+++ GameDashBoard/Aerospike/publishrelease-linux.sh
@@ -2,10 +2,10 @@
 # file: build.sh
 # Builds the safegaming PlayerGenerator application in release mode
 
-dotnet publish GameSimulatorAS.csproj -r linux-x64 -o ".\publish\Release\Aerospike\linux" --self-contained true --configuration Release
+dotnet publish GameDashBoardAS.csproj -r linux-x64 -o ".\publish\Release\Aerospike\linux" --self-contained true --configuration Release
 
 echo "If successful, Published to './publish/Release/aerospike/linux'"
-echo "To run CD to './publish/Release/Aerospike/linux' and execute './GameSimulatorAS -?'"
+echo "To run CD to './publish/Release/Aerospike/linux' and execute './GameDashBoardAS -?'"
 echo "Log files and configuration files will be located in this folder"
 
 

3f2a959fc84d4213d4b550df382e318cce4398db
Richard Andersen <randersenyb@andersenfamily.us>
2023-08-31 04:06:16 +0000

    Updated compile scripts

--- GameDashBoard/Monogodb/publishrelease.sh
+++ GameDashBoard/Monogodb/publishrelease.sh
@@ -2,10 +2,10 @@
 # file: build.sh
 # Builds the safegaming GameSimulator application in release mode
 
-dotnet publish GameSimulatorAS.csproj -a x64 -o ".\publish\Release\Aerospike" --self-contained true --configuration Release
+dotnet publish GameDashBoardMG.csproj -a x64 -o ".\publish\Release\Aerospike" --self-contained true --configuration Release
 
 echo "If successful, Published to './publish/Release/aerospike'"
-echo "To run CD to './publish/Release/Aerospike' and execute './GameSimulatorAS -?'"
+echo "To run CD to './publish/Release/Aerospike' and execute './GameDashBoardMG -?'"
 echo "Log files and configuration files will be located in this folder"
 
 
